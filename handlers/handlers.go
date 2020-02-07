aa813108c79e12a8ff18a723d5e0457b80daf750
Kennedy Nyaga <kennedy.nyaga@qlik.com>
2020-02-07 14:26:44 +0100

    Fix Token key typo

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -37,7 +37,7 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
-	accessToken := req.Header.Get("X-ACCESS_TOKEN")
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
 
 	isTokenValid := data.IsAccessTokenValid(accessToken)
 
@@ -45,11 +45,11 @@ func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 		res.WriteHeader(http.StatusUnauthorized)
 	} else {
 		err := data.AddNewWinner(req.Body)
+
 		if err != nil {
 			res.WriteHeader(http.StatusUnprocessableEntity)
 			return
 		}
-
 		res.WriteHeader(http.StatusCreated)
 	}
 }
